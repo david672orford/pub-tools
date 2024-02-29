785641566ab9b8f9755843bd262324241f7a381e
David Chappell <david672orford@gmail.com>
2024-02-28 23:48:27 -0500

    refactoring, double script exec fix

---
+++ app/subapps/khplayer/static/cameras.js
@@ -0,0 +1,32 @@
+function init_cameras() {
+
+Array.from(document.getElementsByTagName("form")).forEach(form_el => {
+	Array.from(form_el.getElementsByClassName("slider")).forEach(slider => {
+		let input = slider.getElementsByTagName("input")[0];
+		let span = slider.getElementsByTagName("span")[0];
+		span.textContent = input.value;
+		input.addEventListener("input", (event) => {
+			span.textContent = event.target.value;
+			post_ptz(form_el);
+			});
+		});
+	});
+function post_ptz(form_el) {
+	fetch("ptz", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+			},
+		body: JSON.stringify({
+			scene_name: form_el.scene_name.value,
+			id: parseInt(form_el.id.slice(6)),
+			width: parseInt(form_el.width.value),
+			height: parseInt(form_el.height.value),
+			x: parseInt(form_el.x.value),
+			y: parseInt(form_el.y.value),
+			zoom: parseFloat(form_el.zoom.value),
+			})
+		});
+	}
+
+}
