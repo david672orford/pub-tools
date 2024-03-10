a868528edc3503edd0935eccf8b63a413f8151da
David Chappell <david672orford@gmail.com>
2024-03-09 19:23:06 -0500

    my-volume-control: bug fixes, refactoring, mute

--- experiments/my-volume-control.py
+++ experiments/my-volume-control.py
@@ -1,29 +1,38 @@
 #! /usr/bin/python3
-
-# https://athenajc.gitbooks.io/python-gtk-3-api/content/gtk-group/gtkscale.html
-# https://pypi.org/project/pulsectl-asyncio/ 
-# https://github.com/jhenstridge/asyncio-glib/issues/1
-
-# pip3 install asyncio_glib pulsectl_asyncio
+#
+# Custom volume control for PulseAudio
+#
+# ## Currently Used
+# * [Python Gtk+ 3.0 API](https://athenajc.gitbooks.io/python-gtk-3-api/content/)
+# * [Python bindings for Libpulse](https://github.com/mk-fg/python-pulse-control/)
+#
+# ## For the Future
+# * [Python bindings for Libpulse with Async](https://pypi.org/project/pulsectl-asyncio/ 
+# * [GLib event loop integration for asyncio](https://github.com/jhenstridge/asyncio-glib)
+# * [Attempts to integrate above with Gtk](https://github.com/jhenstridge/asyncio-glib/issues/1)
+#
 
 app_name = "my-volume"
 app_icon = "multimedia-volume-control-symbolic"
 app_width = 600
 
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import GLib, Gtk, Gdk
-import signal
-import pulsectl
 import threading
 from time import sleep
 
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+# Load the Gtk GUI toolkit
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import GLib, Gtk, Gdk
 
-# Borderless window always-on-top at top-rigth
+# Interface to PulseAudio
+# pip3 install pulsectl
+import pulsectl
+
+# Borderless window always-on-top at top-right
 class VolumePanel(Gtk.Window):
 	def __init__(self):
 		super().__init__()
+
 		self.set_title(app_name)
 		self.set_decorated(False)
 		self.set_default_size(app_width, 100)
@@ -36,112 +45,157 @@ class VolumePanel(Gtk.Window):
 		self.box.set_margin_end(15)
 		super().add(self.box)
 
-		close_hbox = Gtk.HBox()
+		top_hbox = Gtk.HBox()
+		title = Gtk.Label(label="Volume Controls")
+		top_hbox.pack_start(title, fill=False, expand=False, padding=0)
 		close_button = Gtk.Button(label="X")
-		close_hbox.pack_end(close_button, fill=False, expand=False, padding=0)
-		self.add(close_hbox)
 		close_button.connect("clicked", Gtk.main_quit)
+		top_hbox.pack_end(close_button, fill=False, expand=False, padding=0)
+		self.add(top_hbox)
 
 	def add(self, box):
 		self.box.pack_start(box, expand=False, fill=False, padding=10)
 
 class VolumeControl(Gtk.Frame):
-	def __init__(self, pulse, sink, listener):
-		super().__init__(label=sink.description)
+	def __init__(self, pulse_wrapper, audio_device):
+		self.pulse_wrapper = pulse_wrapper
+		self.audio_device = audio_device
+		super().__init__(label=audio_device.description)
 
 		# Some themes do not render this
 		self.set_shadow_type(Gtk.ShadowType.OUT)
 
-		self.pulse = pulse
-		self.sink = sink
-		self.listener = listener
-
-		volume = sink.volume.value_flat * 100.0
+		volume = audio_device.volume.value_flat * 100.0
 		self.adjustment = Gtk.Adjustment(value=volume, lower=0, upper=153, step_increment=1, page_increment=10, page_size=0)
 		self.scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=self.adjustment)
 		self.scale.set_digits(0)
 		self.scale.add_mark(100, Gtk.PositionType.LEFT, None)
-		self.add(self.scale)
 
-		# Slider changes volume which changes slider. This prevents looping.
-		self.change_lock = False
+		self.mute = Gtk.CheckButton(label="Mute")
+		self.mute.set_active(audio_device.mute)
+
+		box = Gtk.HBox()
+		box.pack_start(self.scale, expand=True, fill=True, padding=0)
+		box.pack_start(self.mute, expand=False, fill=False, padding=0)
+		self.add(box)
+
+		self.expect_slider = 0
+		self.expect_mute = 0
+		self.expect_pulseaudio = 0
 
 		def on_slider_change(target):
-			if not self.change_lock:
-				self.change_lock = True
+			#print("Slider change:", target.get_value())
+			if self.expect_slider > 0:
+				self.expect_slider -= 1
+			else:
+				self.expect_pulseaudio += 1
 				volume = target.get_value()
-				print("Slider change:", volume)
-				self.listener.pause()
-				self.pulse.volume_set_all_chans(self.sink, volume / 100.0)
-				self.listener.resume()
-				self.change_lock = False
+				self.pulse_wrapper.volume_set_all_chans(self.audio_device, volume / 100.0)
 		self.adjustment.connect("value-changed", on_slider_change)
 
+		def on_mute_checkbox_changed(target):
+			#print("Mute toggled", self.mute.get_active())
+			if self.expect_mute > 0:
+				self.expect_mute -= 1
+			else:
+				self.expect_pulseaudio += 1
+				self.pulse_wrapper.set_mute(self.audio_device, self.mute.get_active())
+		self.mute.connect("toggled", on_mute_checkbox_changed)
+
+		# PulseAudio reports the state of the source or audio_device has changed
 		def on_volume_change(info):
-			if not self.change_lock:
-				self.change_lock = True
+			#print("PulseAudio change:", info, info.mute, info.volume.value_flat)
+			if self.expect_pulseaudio > 0:
+				self.expect_pulseaudio -= 1
+			else:
+				self.expect_slider += 1
 				GLib.idle_add(lambda: _on_volume_change(info))
 		def _on_volume_change(info):
-			print("Volume change:", info.mute, info.volume.value_flat)
+			#print("_PulseAudio change:", info.mute, info.volume.value_flat)
 			volume = info.volume.value_flat * 100.0
 			self.adjustment.set_value(volume)
-			self.change_lock = False
-		listener.listeners[sink.index] = on_volume_change	
+			if info.mute != self.mute.get_active():
+				self.expect_mute += 1
+				self.mute.set_active(info.mute)
+		pulse_wrapper.subscribe(audio_device, on_volume_change)
 
-class PulseListener:
+class PulseWrapper:
 	def __init__(self, pulse):
 		self.pulse = pulse
-		self.listeners = {}
-		self.event = None
-		self.paused = False
-		self.listening = False
+		self._subscribers = {}
+		self._event = None				# event currently being processed
+		self._listener_paused = False
+		self._in_listener = False
+
 		def queue_event(event):
-			self.event = event
+			self._event = event
 			raise pulsectl.PulseLoopStop
-		pulse.event_mask_set("all")
+		pulse.event_mask_set("source", "sink")
 		pulse.event_callback_set(queue_event)
-	def run(self):
+
+	def subscribe(self, audio_device, callback):
+		self._subscribers[audio_device.index] = callback
+
+	def run_event_listener(self):
 		while True:
-			self.listening = True
+			self._in_listener = True
 			self.pulse.event_listen(timeout=60)
-			self.listening = False
-			if self.event is not None:
-				node_index = self.event.index
-				if node_index in self.listeners:
+			self._in_listener = False
+			if self._event is not None:
+				print("event:", self._event, self._event.facility)
+				node_index = self._event.index
+				if node_index in self._subscribers:
 					try:
-						info = self.pulse.sink_info(node_index)
-						self.listeners[node_index](info)
+						if self._event.facility == pulsectl.PulseEventFacilityEnum.source:
+							info = self.pulse.source_info(node_index)
+						else:
+							info = self.pulse.sink_info(node_index)
+						self._subscribers[node_index](info)
 					except pulsectl.PulseIndexError:
 						print("PulseIndexError!")
-				self.event = None
-			while self.paused:
+				self._event = None
+			while self._listener_paused:
 				sleep(1.0)
-	def pause(self):
-		self.paused = True
-		while self.listening:
+
+	def _pause_listener(self):
+		self._listener_paused = True
+		while self._in_listener:
 			self.pulse.event_listen_stop()
 			sleep(0.01)
-	def resume(self):
-		self.paused = False
+
+	def _resume_listener(self):
+		self._listener_paused = False
+
+	def volume_set_all_chans(self, audio_device, volume):
+		self._pause_listener()
+		self.pulse.volume_set_all_chans(audio_device, volume)
+		self._resume_listener()
+
+	def set_mute(self, audio_device, mute:bool):
+		print("set_mute:", mute)
+		self._pause_listener()
+		if type(audio_device) is pulsectl.PulseSourceInfo:
+			self.pulse.source_mute(audio_device.index, mute)
+		else:
+			self.pulse.sink_mute(audio_device.index, mute)
+		self._resume_listener()
 
 def main():
 	panel = VolumePanel()
 
 	pulse = pulsectl.Pulse(app_name, threading_lock=True)
-	#pulse.connect()
+	pulse_wrapper = PulseWrapper(pulse)
 
-	listener = PulseListener(pulse)
-
-	for node in pulse.sink_list() + pulse.source_list():
-		print(node)
-		if not node.name.endswith(".monitor"):
-			control = VolumeControl(pulse, node, listener)
+	for audio_device in pulse.sink_list() + pulse.source_list():
+		print(type(audio_device), audio_device)
+		if not audio_device.name.endswith(".monitor"):
+			control = VolumeControl(pulse_wrapper, audio_device)
 			panel.add(control)
 
 	for client in pulse.client_list():
 		print("client:", client)
 
-	thread = threading.Thread(target=lambda: listener.run())
+	thread = threading.Thread(target=lambda: pulse_wrapper.run_event_listener())
 	thread.daemon = True
 	thread.start()
 
