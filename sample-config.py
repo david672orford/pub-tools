81f0a670fb44b466e538bd8709a5a0b6824610f3
David Chappell <david672orford@gmail.com>
2024-10-07 00:19:13 -0400

    config: updated sample

--- sample-config.py
+++ sample-config.py
@@ -1,6 +1,11 @@
 # Set to a random value
 SECRET_KEY = "__change_me__"
 
+# Select theme. Default is "basic-light".
+#THEME="basic-light"
+#THEME="basic-dark"
+#THEME="colorful"
+
 # Pub-Tools has several applications which can be enabled individually
 ENABLED_SUBAPPS = [
 	"khplayer",
@@ -16,19 +21,14 @@ OBS_WEBSOCKET = {
 	"password": "__change_me__",
 	}
 
-# Select theme. Default is "basic-light".
-#THEME="basic-light"
-#THEME="basic-dark"
-#THEME="colorful"
-
-# Language for user interface
-#UI_LANGUAGE="en"
+# Language for user interface (default is "en")
+#UI_LANGUAGE="ru"
 
 # Size of text in browser dock may be off
 #OBS_BROWSER_DOCK_SCALE = 1.0
 
-# Language for loading publications, videos
-#PUB_LANGUAGE="en"
+# Language for loading publications, videos (default is "en")
+#PUB_LANGUAGE="ru"
 
 # Uncomment to enable subtitles in videoes, where available
 #SUB_LANGUAGE="en"
@@ -36,12 +36,25 @@ OBS_WEBSOCKET = {
 # Set to "240p", "360p", "480p", or "720p"
 #VIDEO_RESOLUTION="480p"
 
-VIDEO_REMOTES = {
+# Patchbay mode:
+# False - Disable
+# True - Enable
+# "virtual-cable" - Enable and enable virtual cable controls
+#PATCHBAY = "cable"
+
+# Assign OBS source names to V4L2 devices
+#CAMERA_NAME_OVERRIDES = {
+#	"CREALITY CAM: HD 4MP WEBCAM": "Good Webcam",
+#	"HD USB Camera: HD USB Camera": "ELP 2.0 MP Webcam",
+#	}
+
+# Sources using VDO.Ninja
+#VIDEO_REMOTES = {
 #	"Main Hall": {
 #		"view": "my-channel-123",
 #	},
-#	"Other Remote": {
+#	"Other Hall": {
 #		"view": "my-channel-124",
 #	},
-}
+#}
 
